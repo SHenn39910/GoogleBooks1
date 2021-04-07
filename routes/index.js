fcec43fabbbda58a72937078462a720b72f440f0
Sonja Hennessy <sonjahennessy15@gmail.com>
2021-04-07 15:03:44 -0400

    created routes for api

--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,10 @@ const apiRoutes = require("./api");
 // API Routes
 router.use("/api", apiRoutes);
 
+// If no API routes are hit, send the React App
+router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+
+});
+
+module.exports = router;
